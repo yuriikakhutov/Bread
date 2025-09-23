11f653848c3eb743c33d3cdd7ab7e553a9990054
yuriikakhutov <167523400+yuriikakhutov@users.noreply.github.com>
2025-09-23 23:18:37 +0200

    Reduce invite delay to 1.5 seconds

--- core/src/main/java/com/rtm516/mcxboxbroadcast/core/FriendManager.java
+++ core/src/main/java/com/rtm516/mcxboxbroadcast/core/FriendManager.java
@@ -259,7 +259,7 @@ public class FriendManager {
 
                         sendInvite(person.xuid, person.gamertag != null ? person.gamertag : person.displayName);
                         try {
-                            TimeUnit.SECONDS.sleep(5);
+                            TimeUnit.MILLISECONDS.sleep(1500);
                         } catch (InterruptedException interruptedException) {
                             Thread.currentThread().interrupt();
                             return;
